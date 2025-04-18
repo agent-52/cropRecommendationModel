9af71c5e27e87944e6fb282e819e00ed5b2cdd89
agent-52 <abhaybhadauriya52@gmail.com>
2025-04-18 17:17:16 +0530

    fix the scikit version

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ def recommend_crop_endpoint():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
-    # app.run(debug=True)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
